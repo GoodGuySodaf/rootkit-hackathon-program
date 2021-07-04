9bc92d446f7aeda38c469e5e39f378a69a3c8bbe
TazMatic <31835653+TazMatic@users.noreply.github.com>
2021-07-04 13:51:14 -0500

    Add notes

--- hackathon.py
+++ hackathon.py
@@ -2,6 +2,9 @@ import sys
 import os
 import subprocess
 
+# Note Curl opens a tcp connection with the server and does a get
+# request. Curl then cleanly tears down the connection.
+
 #execute command in shell (cmd)
 subprocess = subprocess.Popen("curl hack.rootkit.education:2870", shell=True, stdout=subprocess.PIPE)
 
@@ -11,6 +14,8 @@ print(subprocess_return)
 
 #take the numbers from the shell output and calculate
 result = int(str(subprocess_return).split()[11]) + int(str(subprocess_return).split()[13])
+
+# Note this creates a new shell session and sends the integer as a command
 
 #input the result to shell
 stream = os.popen(str(result))
