f02f12fa6dd9bdfc0d45e07e4e5e7783763fc78b
GoodGuySodaf <69467094+GoodGuySodaf@users.noreply.github.com>
2021-07-04 16:04:45 +0700

    Add files via upload

---
+++ hackathon.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import subprocess
+
+#execute command in shell (cmd)
+subprocess = subprocess.Popen("curl hack.rootkit.education:2870", shell=True, stdout=subprocess.PIPE)
+
+#read and print shell output
+subprocess_return = subprocess.stdout.readlines()
+print(subprocess_return)
+
+#take the numbers from the shell output can calculate
+code = int(str(subprocess_return).split()[11]) + int(str(subprocess_return).split()[13])
+
+#input the result to shell
+stream = os.popen(str(code))
+
+#print out shell respond
+output = stream.read()
+print(output)
+
